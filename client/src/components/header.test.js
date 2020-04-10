f5f416f89ab5a0fbd2406db9814c0c11fe1d347b
tippitytapp <60143534+tippitytapp@users.noreply.github.com>
2020-04-10 13:40:36 -0400

    working on the tests got some working

---
+++ client/src/components/header.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {render, fireEvent} from "@testing-library/react"
+import Header from "./header";
+
+test('header is visible', () => {
+    //Arrange
+    const {getByText} = render(<Header />)
+    const headerText = getByText(/women's world cup players/i)
+    //ACT 
+    //NONE
+    //ASSERT
+    expect(headerText).toBeVisible();
+})
+
+test('darkmode button visible', () => {
+    //ARRANGE
+    const {getByText} = render(<Header />)
+    const darkButton = getByText(/dark mode/i)
+    //ASSERT
+    expect(darkButton).toBeVisible();
+})
+
